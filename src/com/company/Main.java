dddfe433ebe57468a84966c403571f0daedb60d3
jukegbu1 <jukegbu1@gmail.com>
2019-09-10 10:30:48 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Write a Java program to print a box, an oval, or a diamond (choose one) using asterisks.
+        System.out.println("*********");
+        System.out.println("*" + "\t" + "\t" + "*");
+        System.out.println("*" + "\t" + "\t" + "*");
+        System.out.println("*" + "\t" + "\t" + "*");
+        System.out.println("*" + "\t" + "\t" + "*");
+        System.out.println("*********");
+
+    }
+}
